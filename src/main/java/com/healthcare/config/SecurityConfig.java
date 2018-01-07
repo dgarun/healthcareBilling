e65c439dc992702054cab2873bc9241d88aeefb5
godson <godsonherpatt@gmail.com>
2018-01-07 22:22:19 +0530

    remove secuirty and change class file for login change

--- src/main/java/com/healthcare/config/SecurityConfig.java
+++ src/main/java/com/healthcare/config/SecurityConfig.java
@@ -1,28 +1,9 @@
 package com.healthcare.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
-@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
-	/*@Override
-	@Autowired
-	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("user")
-				.password("user").roles("USER");
-	}
-
-	@Override
-	public void configure(HttpSecurity http) throws Exception {
-		http.antMatcher("/**").authorizeRequests().anyRequest().hasRole("USER")
-				.and().formLogin().loginPage("/login.jsp")
-				.failureUrl("/login.jsp?error=1").loginProcessingUrl("/login")
-				.permitAll().and().logout()
-				.logoutSuccessUrl("/listEmployees.html");
-
-	}*/
+	
 	
 }
